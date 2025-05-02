bf6cf2420ba5a1e7ed5f4bb9620a540577fbd7a4
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-05-02 14:25:01 +0200

    feat: add SelectBox UI component

---
+++ app/_components/selectBox/selectBox.tsx
@@ -0,0 +1,39 @@
+import { Select } from '@base-ui-components/react/select';
+import chevronIcon from '@public/chevron-up-icon.svg';
+import Image from 'next/image';
+import styles from './selectBox.module.css';
+import React from 'react';
+
+type SelectBoxProps = {
+    placeholder?: string;
+    items: { label: string; value: string }[];
+    className?: { trigger?: string; popup?: string };
+};
+
+export default function SelectBox({ placeholder, items, className }: SelectBoxProps) {
+    return (
+        <Select.Root alignItemToTrigger={false} modal={false}>
+            <Select.Trigger className={`${styles.trigger} ${className?.trigger}`}>
+                <Select.Value placeholder={placeholder ?? 'Tout'} />
+                <Select.Icon>
+                    <Image src={chevronIcon} className={styles.chevronIcon} alt="chevron" />
+                </Select.Icon>
+            </Select.Trigger>
+            <Select.Portal>
+                <Select.Positioner side="top" align="end">
+                    <Select.Popup className={`${styles.popup} ${className?.popup}`}>
+                        {items.map(item => (
+                            <Select.Item
+                                className={styles.item}
+                                key={item.value}
+                                value={item.value}
+                            >
+                                <Select.ItemText>{item.label}</Select.ItemText>
+                            </Select.Item>
+                        ))}
+                    </Select.Popup>
+                </Select.Positioner>
+            </Select.Portal>
+        </Select.Root>
+    );
+}
