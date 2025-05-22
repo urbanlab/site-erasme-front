a29bfd3145f921229443dc26ddcded96b0980ff5
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-05-22 11:11:59 +0200

    feat: add 'mentions-legales' page
    - created new graphql query
    - updated remoteHtml component to accept className as parameter

---
+++ app/(footer)/mentions-legales/page.tsx
@@ -0,0 +1,26 @@
+import { MENTIONS_LEGALES } from '@graphql/queries';
+import heroImage from '@public/hero-img.svg';
+import { query } from '@services/apollo/apolloClient';
+import ShapedImage from '@ui/components/shapedImage';
+import styles from './page.module.css';
+import RemoteHtml from '@services/remoteHtml';
+
+export default async function MentionsLegales() {
+    const { data } = await query({
+        query: MENTIONS_LEGALES,
+        variables: { id: parseInt(process.env.SPIP_PAGE_MENTIONS_LEGALES_ID ?? '') },
+    });
+
+    return (
+        <div className={`${styles.mainContainer} ${styles.localVariables}`}>
+            <ShapedImage
+                className={styles.logo}
+                alt="logo rubrique"
+                maskShape="wide"
+                src={data.getArticle?.logo ?? heroImage}
+            />
+            <h1 className={styles.title}>{data.getArticle?.titre}</h1>
+            {data.getArticle?.texte && <RemoteHtml html={data.getArticle?.texte} className={styles.content} />}
+        </div>
+    );
+}
--- app/_services/_remoteHtml/remoteHtml.tsx
+++ app/_services/_remoteHtml/remoteHtml.tsx
@@ -1,8 +1,8 @@
 import DOMPurify from 'isomorphic-dompurify';
 import styles from './remoteHtml.module.css';
 
-export default function RemoteHtml({ html }: { html: string }) {
+export default function RemoteHtml({ html, className }: { html: string, className?: string }) {
     const sanitizedHtml = DOMPurify.sanitize(html, { ADD_TAGS: ['iframe'] });
 
-    return <div className={styles.remoteHtml} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
+    return <div className={`${styles.remoteHtml} ${className}`} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
 }
