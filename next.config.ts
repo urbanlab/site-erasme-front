1456d795c492c7104733b10cba938183d941c425
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-06-16 11:43:28 +0200

    refacto: update environment variables for backend URL and GraphQL endpoint
    - whitelist image host url
    - update README

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'standalone',
+    images: {
+        remotePatterns: [new URL(`${process.env.BACKEND_BASE_URL}/IMG/**`)],
+    },
 };
 
 export default nextConfig;
