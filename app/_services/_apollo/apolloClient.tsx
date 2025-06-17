05d8411ebdd4b20c3f6c9cbac67b8c313e9f1a89
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-06-17 17:30:23 +0200

    fix: rollback ApolloClient configuration

--- app/_services/_apollo/apolloClient.tsx
+++ app/_services/_apollo/apolloClient.tsx
@@ -2,21 +2,18 @@ import { HttpLink } from '@apollo/client';
 import { registerApolloClient, ApolloClient, InMemoryCache } from '@apollo/client-integration-nextjs';
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
-    const uri: string = `${process.env.BACKEND_BASE_URL}/${process.env.GRAPHQL_ENDPOINT}`;
-    const token: string = process.env.GRAPHQL_TOKEN ?? '';
-
     return new ApolloClient({
         cache: new InMemoryCache(),
         link: new HttpLink({
             // this needs to be an absolute url, as relative urls cannot be used in SSR
-            uri: uri,
+            uri: `${process.env.BACKEND_BASE_URL}/${process.env.GRAPHQL_ENDPOINT}`,
             fetchOptions: {
                 // you can pass additional options that should be passed to `fetch` here,
                 // e.g. Next.js-related `fetch` options regarding caching and revalidation
                 // see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
             },
             headers: {
-                'X-Auth-Token': token,
+                'X-Auth-Token': process.env.GRAPHQL_TOKEN ?? '',
             },
         }),
     });
