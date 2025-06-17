fbebfab1b81f54afb3dfa146d7b34d6911184ae1
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-06-17 17:05:27 +0200

    refacto: extract variables to outside of the function on ApolloClient configuration

--- app/_services/_apollo/apolloClient.tsx
+++ app/_services/_apollo/apolloClient.tsx
@@ -2,18 +2,21 @@ import { HttpLink } from '@apollo/client';
 import { registerApolloClient, ApolloClient, InMemoryCache } from '@apollo/client-integration-nextjs';
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+    const uri: string = `${process.env.BACKEND_BASE_URL}/${process.env.GRAPHQL_ENDPOINT}`;
+    const token: string = process.env.GRAPHQL_TOKEN ?? '';
+
     return new ApolloClient({
         cache: new InMemoryCache(),
         link: new HttpLink({
             // this needs to be an absolute url, as relative urls cannot be used in SSR
-            uri: `${process.env.BACKEND_BASE_URL}/${process.env.GRAPHQL_ENDPOINT}`,
+            uri: uri,
             fetchOptions: {
                 // you can pass additional options that should be passed to `fetch` here,
                 // e.g. Next.js-related `fetch` options regarding caching and revalidation
                 // see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
             },
             headers: {
-                'X-Auth-Token': process.env.GRAPHQL_TOKEN ?? '',
+                'X-Auth-Token': token,
             },
         }),
     });
