b3d754649133bd803f083ee93d165b144f9c5b91
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-06-25 18:29:26 +0200

    feat: enhance Button and InputField components with additional props for improved functionality

--- app/_ui/_elements/button/button.tsx
+++ app/_ui/_elements/button/button.tsx
@@ -6,15 +6,25 @@ type ButtonProps = {
     onClick?: () => void;
     className?: string;
     ref?: React.Ref<HTMLButtonElement>;
+    type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 } & React.HTMLAttributes<HTMLElement>;
 
-export default function Button({ children, variant, onClick, className, ref, ...inheritedProps }: ButtonProps) {
+export default function Button({
+    children,
+    variant,
+    onClick,
+    className,
+    ref,
+    type = 'button',
+    ...inheritedProps
+}: ButtonProps) {
     return (
         <button
             {...inheritedProps}
             ref={ref}
             className={`${styles.button} ${className} ${styles[variant]}`}
             onClick={onClick}
+            type={type}
         >
             {children}
         </button>
--- app/_ui/_elements/inputField/inputField.tsx
+++ app/_ui/_elements/inputField/inputField.tsx
@@ -3,19 +3,25 @@ import styles from './inputField.module.css';
 
 type InputFieldProps = {
     type: 'text' | 'email' | 'password';
+    name: string;
+    value?: string;
     placeholder?: string;
     className?: string;
     ref?: React.Ref<HTMLInputElement>;
+    required?: boolean;
 } & React.HTMLAttributes<HTMLElement>;
 
-export default function InputField({ type, placeholder, className, ref, ...inheritedProps }: InputFieldProps) {
+export default function InputField({ type, name, value, placeholder, className, ref, required = false, ...inheritedProps }: InputFieldProps) {
     return (
         <Input
-            {...inheritedProps}
-            ref={ref}
-            className={`${styles.input} ${className} ${styles[type]}`}
             type={type}
+            name={name}
+            value={value}
             placeholder={placeholder}
+            className={`${styles.input} ${className} ${styles[type]}`}
+            ref={ref}
+            {...inheritedProps}
+            required={required}
         />
     );
 }
