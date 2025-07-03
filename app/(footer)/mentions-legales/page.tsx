f1034a1fde7e35dbd834735877bacbf8e469d4dd
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-07-03 10:53:39 +0200

    fix: missing file from commit b2a923de3bf4cfc6fc2d677e9a025b3c0b0d2fdb

--- app/(footer)/mentions-legales/page.tsx
+++ app/(footer)/mentions-legales/page.tsx
@@ -1,13 +1,13 @@
 import { ArticleBasicQuery } from '@graphql/__generated__/graphql';
 import { ARTICLE_BASIC } from '@graphql/queries';
 import heroImage from '@public/hero-img.svg';
-import { query } from '@services/apollo/apolloClient';
+import { getClient } from '@services/apollo/apolloClient';
 import RemoteHtml from '@services/remoteHtml';
 import ShapedImage from '@ui/components/shapedImage';
 import styles from './page.module.css';
 
 export default async function MentionsLegales() {
-    const { data } = await query<ArticleBasicQuery>({
+    const { data } = await getClient().query<ArticleBasicQuery>({
         query: ARTICLE_BASIC,
         variables: { id: parseInt(process.env.SPIP_ARTICLE_MENTIONS_LEGALES_ID ?? '') },
     });
