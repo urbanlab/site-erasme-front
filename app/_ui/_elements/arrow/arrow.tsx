9a226828652702cd4f910a1927363794207b33df
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-07-07 16:10:02 +0200

    feat: simplify Arrow UI component

--- app/_ui/_elements/arrow/arrow.tsx
+++ app/_ui/_elements/arrow/arrow.tsx
@@ -1,6 +1,6 @@
 import styles from './arrow.module.css';
 
-const ArrowIcon = ({size}: {size?: number}) => (
+const ArrowIcon = ({ size }: { size?: number }) => (
     <svg
         width={size ?? '80%'}
         height={size ?? '80%'}
@@ -8,21 +8,31 @@ const ArrowIcon = ({size}: {size?: number}) => (
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <path d="M2.3284 15.7842L26.9246 15.8589" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M16.6621 2.19922L29.4238 15.6639L15.0904 29.249" stroke="currentColor" strokeWidth="4" strokeLinecap="round"/>
-  </svg>
+        <path
+            d="M2.3284 15.7842L26.9246 15.8589"
+            stroke="currentColor"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <path
+            d="M16.6621 2.19922L29.4238 15.6639L15.0904 29.249"
+            stroke="currentColor"
+            strokeWidth="4"
+            strokeLinecap="round"
+        />
+    </svg>
 );
 
 type Orientation = 'northeast' | 'southwest' | 'northwest' | 'southeast' | 'north' | 'south' | 'east' | 'west';
 
 type ArrowProps = {
     orientation: Orientation;
-    hoverOrientation?: Orientation;
     className?: string;
     size?: number;
 };
 
-export default function Arrow({ orientation, hoverOrientation, size, className }: ArrowProps) {
+export default function Arrow({ orientation, size, className }: ArrowProps) {
     const arrowRotation = (orientation: Orientation) => {
         switch (orientation) {
             case 'north':
@@ -48,7 +58,6 @@ export default function Arrow({ orientation, hoverOrientation, size, className }
 
     const arrowOrientationStyle: React.CSSProperties = {
         ['--orientation' as string]: arrowRotation(orientation),
-        ['--hover-orientation' as string]: arrowRotation(hoverOrientation || orientation),
     };
 
     return (
