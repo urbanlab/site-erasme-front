2d94d755bbdfa65d3cf2968beea359dbcf52024e
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-07-07 15:21:19 +0200

    feat: enhance RemoteHtml component to clean malformed tags and support text length limits

--- app/_services/_remoteHtml/remoteHtml.tsx
+++ app/_services/_remoteHtml/remoteHtml.tsx
@@ -1,8 +1,32 @@
 import DOMPurify from 'isomorphic-dompurify';
 import styles from './remoteHtml.module.css';
 
-export default function RemoteHtml({ html, className }: { html: string, className?: string }) {
-    const sanitizedHtml = DOMPurify.sanitize(html, { ADD_TAGS: ['iframe'] });
+/**
+ * Remove <br> inside tag openings like <div ... <br> ...>
+ * */
+const cleanMalformedTags = (input: string): string => {
+    return input.replace(/<([a-z]+)([^>]*?)<br[^>]*>([^>]*)>/gi, '<$1$2$3');
+};
 
-    return <div className={`${styles.remoteHtml} ${className}`} dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />;
+export default function RemoteHtml({
+    html,
+    returnOnlyText = false,
+    textReturnLength = -1,
+    className,
+}: {
+    html: string;
+    returnOnlyText?: boolean;
+    textReturnLength?: number;
+    className?: string;
+}) {
+    const cleanedHtml = cleanMalformedTags(html);
+
+    const sanitizedHtml = DOMPurify.sanitize(
+        cleanedHtml,
+        returnOnlyText ? { ALLOWED_ATTR: [], ALLOWED_TAGS: ['p'] } : { ADD_TAGS: ['iframe'] }
+    );
+
+    const slicedHtml = textReturnLength !== -1 ? `${sanitizedHtml.slice(0, textReturnLength)}...` : sanitizedHtml;
+
+    return <div className={`${styles.remoteHtml} ${className}`} dangerouslySetInnerHTML={{ __html: slicedHtml }} />;
 }
