c6bdb7fbc94b9f0b3a48688ce80a9ae38bf67e49
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-07-23 16:51:28 +0200

    fix: font size and color on project list component

--- app/_ui/_components/rubriqueProjetsAccordion/rubriqueProjetsAccordion.tsx
+++ app/_ui/_components/rubriqueProjetsAccordion/rubriqueProjetsAccordion.tsx
@@ -27,7 +27,7 @@ export default function RubriqueProjetsAccordion({
                 return (
                     <Accordion.Item key={rubrique.id} className={styles.item}>
                         <Accordion.Trigger className={styles.trigger}>
-                            <div className={styles.title}>{rubrique?.titre}</div>
+                            <p className={styles.title}>{rubrique?.titre}</p>
                             <Image src={chevronIcon} alt="" className={styles.arrow} />
                         </Accordion.Trigger>
                         <Accordion.Panel className={styles.panel}>
